44111ecdeaf60735370710709442d3b1634e095a
eschillus <eskil@darpe.se>
2022-04-13 18:02:49 +0200

    Updated

--- config.h
+++ config.h
@@ -3,29 +3,24 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel size of windows */
-static const unsigned int gappx     = 8;        /* gaps size between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 8;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=12" };
-static const char dmenufont[]       = "Hack:size=12";
-//background color
-static const char normbgcolor[]     = "#201d19";
-static const char normbordercolor[] = "#b36d36";
-static const char normfgcolor[]     = "#bcaba2";
-static const char selfgcolor[]      = "#bcaba2";
-static const char selbordercolor[]  = "#b36d36";
-static const char selbgcolor[]      = "#201d19";
+static const char *fonts[]          = { "Liberation Mono:size=12" };
+static const char dmenufont[]       = "Liberation Mono:size=12";
+static const char col_gray1[]       = "#212224";
+static const char col_gray3[]       = "#C6C7C8";
+static const char col_cyan[]        = "#81A2BE";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+	/*               fg         bg          border   */
+	[SchemeNorm] = { col_gray3, col_gray1,  col_gray1 },
+	[SchemeSel]  = { col_gray3, col_gray1,  col_cyan  },
 };
 
 /* tagging */
-//tag names (upper left)
-static const char *tags[] = { "-", "-", "-" };
+static const char *tags[] = { "1", "2", "3" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -33,8 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
@@ -62,54 +56,43 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", normbgcolor, "-sf", selfgcolor, NULL };
-//static const char *filemanager[] = { "
-//
-//launches htop
-static const char *monitor[] = { "/usr/bin/htop", NULL };
-
-//sets st as the default terminal
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-#include "shiftview.c"
-static char *endx[] = { "/bin/sh", "-c", "endx", "externalpipe", NULL };
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_t,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_z,	   zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ 0,                            XF86XK_AudioLowerVolume, spawn, SHCMD("pamixer --allow-boost -d 5") },	
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn, SHCMD("pamixer --allow-boost -i 5") },
-	{ 0,                            XF86XK_AudioMute,        spawn, SHCMD("pamixer -t") },
-	{ MODKEY,              		    XK_n,      shiftview,  	   { .i = +1 } },
-	{ MODKEY,              		    XK_b,      shiftview,      { .i = -1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier              key                      function        argument */
+	{ MODKEY,                XK_d,                    spawn,          {.v = dmenucmd } },
+	{ MODKEY,                XK_Return,               spawn,          {.v = termcmd } },
+	{ MODKEY,                XK_b,                    togglebar,      {0} },
+	{ MODKEY,                XK_j,                    focusstack,     {.i = +1 } },
+	{ MODKEY,                XK_k,                    focusstack,     {.i = -1 } },
+	{ MODKEY,                XK_i,                    incnmaster,     {.i = +1 } },
+	{ MODKEY,                XK_d,                    incnmaster,     {.i = -1 } },
+	{ MODKEY,                XK_h,                    setmfact,       {.f = -0.05} },
+	{ MODKEY,                XK_l,                    setmfact,       {.f = +0.05} },
+	{ MODKEY,                XK_Tab,                  view,           {0} },
+	{ MODKEY,                XK_q,                    killclient,     {0} },
+	{ MODKEY,                XK_t,                    setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                XK_f,                    setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                XK_m,                    setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                XK_space,                setlayout,      {0} },
+	{ MODKEY|ShiftMask,      XK_space,                togglefloating, {0} },
+	{ MODKEY,                XK_0,                    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,      XK_0,                    tag,            {.ui = ~0 } },
+	{ MODKEY,                XK_comma,                focusmon,       {.i = -1 } },
+	{ MODKEY,                XK_period,               focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,      XK_comma,                tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,      XK_period,               tagmon,         {.i = +1 } },
+	{ MODKEY,                XK_minus,                setgaps,        {.i = -1 } },
+	{ MODKEY,                XK_equal,                setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,      XK_equal,                setgaps,        {.i = 0  } },
+	{ 0,                     XF86XK_AudioLowerVolume, spawn,          SHCMD("pamixer --allow-boost -d 5")},
+	{ 0,                     XF86XK_AudioRaiseVolume, spawn,          SHCMD("pamixer --allow-boost -i 5")},
+	{ MODKEY|ShiftMask,      XK_q,                    quit,           {0} },
+	TAGKEYS(                 XK_1,                                    0)
+	TAGKEYS(                 XK_2,                                    1)
+	TAGKEYS(                 XK_3,                                    2)
+
 };
 
 /* button definitions */
@@ -128,4 +111,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
